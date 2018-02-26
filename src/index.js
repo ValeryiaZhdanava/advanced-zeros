7ca93791284c14b2ef22751b8dbd5e3a64e92a37
Valeryia Zhdanava <vzhd@ciklum.com>
2018-02-26 15:37:04 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let max;
+    next: for (let i = 2; i <= base; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                continue next;
+            }
+        }
+        if (base % i === 0) {
+            max = i;
+        }
+    }
+    let count = 0;
+    while (number > 0) {
+        number = Math.floor(number / max);
+        count += number;
+    }
+    return count;
 }
